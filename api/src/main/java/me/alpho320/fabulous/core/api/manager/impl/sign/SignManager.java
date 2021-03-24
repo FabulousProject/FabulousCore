0535836456701199e506ec112f553076a3cf45a6
Alpho320 <cahitbaba00@gmail.com>
2021-03-24 04:56:32 +0300

    1.0.3 - Manager of WorldBorderManager added & some useful methods added to SignManager.

--- api/src/main/java/me/alpho320/fabulous/core/api/manager/impl/sign/SignManager.java
+++ api/src/main/java/me/alpho320/fabulous/core/api/manager/impl/sign/SignManager.java
@@ -1,12 +1,18 @@
 package me.alpho320.fabulous.core.api.manager.impl.sign;
 
 import me.alpho320.fabulous.core.api.manager.IManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.UUID;
 
 public interface SignManager extends IManager {
 
-    Map<UUID, SignGUI> guiMap();
+    @NotNull Map<UUID, SignGUI> guiMap();
+
+    @Nullable SignGUI getSign(UUID id);
+    @Nullable SignGUI getSign(String id);
+    @Nullable SignGUI getSign(Object player);
 
 }
---
+++ api/src/main/java/me/alpho320/fabulous/core/api/manager/impl/worldborder/WorldBorderManager.java
@@ -0,0 +1,10 @@
+package me.alpho320.fabulous.core.api.manager.impl.worldborder;
+
+import me.alpho320.fabulous.core.api.manager.IManager;
+
+public interface WorldBorderManager extends IManager {
+
+
+
+
+}
