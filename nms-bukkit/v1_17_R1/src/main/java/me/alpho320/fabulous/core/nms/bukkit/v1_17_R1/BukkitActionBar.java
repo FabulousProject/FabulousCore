5c3bc39d6fceba26e2113845b1c471bcd4c46e4d
Cahit Şahin <cahitbaba00@gmail.com>
2022-03-08 22:32:43 +0300

    1.1.45 - fix for 1.17 & java 17.

--- nms-bukkit/v1_17_R1/src/main/java/me/alpho320/fabulous/core/nms/bukkit/v1_17_R1/BukkitActionBar.java
+++ nms-bukkit/v1_17_R1/src/main/java/me/alpho320/fabulous/core/nms/bukkit/v1_17_R1/BukkitActionBar.java
@@ -1,18 +1,15 @@
 package me.alpho320.fabulous.core.nms.bukkit.v1_17_R1;
 
 import me.alpho320.fabulous.core.api.manager.impl.message.IActionBar;
-import net.minecraft.network.chat.ChatMessageType;
-import net.minecraft.network.chat.IChatBaseComponent;
-import net.minecraft.network.protocol.game.PacketPlayOutChat;
-import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
 public class BukkitActionBar implements IActionBar {
 
     @Override
     public void send(Object player, String text) {
-        PacketPlayOutChat packet = new PacketPlayOutChat(IChatBaseComponent.ChatSerializer.a("{\"text\":\"" + text + "\"}"), ChatMessageType.c, ((Player)player).getUniqueId());
-        ((CraftPlayer) player).getHandle().b.sendPacket(packet);
+        ((Player) player).spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(text));
     }
 
 }
